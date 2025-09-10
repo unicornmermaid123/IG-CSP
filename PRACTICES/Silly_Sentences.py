83eabdb67653087dc55c1366017bf9273617b662
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-09-10 13:42:44 -0600

    started silly sentences

---
+++ PRACTICES/Silly_Sentences.py
@@ -0,0 +1,11 @@
+# IG 6th Silly Sentences
+name = input(" give me a name")
+print("thank you")
+food = input("give me a food")
+print("thank you")
+body_part = input("give me a bady part")
+print("thank you")
+place = input("give me a place")
+print("thank you")
+
+print("my very rude siter", name ,"gave me moldy" , food , ) 
