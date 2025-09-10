aee07b0761a61f29e546559cc5c55bc9a7779fbb
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-09-10 13:32:02 -0600

    name docorator

---
+++ PRACTICES/Name_Decorator.py
@@ -0,0 +1,7 @@
+# IG 6th Name decorator
+name = input("what is your name")
+print("hello "+ name + " welome")
+print("here is some name decor!")
+print("*+" + name + " +*")
+print("^_^" + name + " ^_^")
+
