0436f488bd152cd070cf945468cfce76e5c5b2c7
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-09-19 13:43:39 -0600

    loop notes

---
+++ PRACTICES/time_of_day.py
@@ -0,0 +1,12 @@
+# IG 6th time of day
+
+time = int(input("what is the time?: "))
+
+if time <= 0: 
+    print("Good Morning")
+
+elif time <= 12: 
+    print("Good Afternoon")   
+
+elif time <= 21:
+    print("Good Night")    
---
+++ notes/loop_notes.py
@@ -0,0 +1,11 @@
+# IG 6th Loop Notes
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+print(f" the time is: {current} ")
+print(f"the hour is: {hour}")
+
+#list
+siblings = ["mariel"]
