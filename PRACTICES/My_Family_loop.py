e2758391306c56357ba0e7abed7cbd1bda353202
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-09-23 13:09:37 -0600

    family loop

---
+++ PRACTICES/My_Family_loop.py
@@ -0,0 +1,14 @@
+#IG 6th my family loop notes
+
+family = ["karen", "raul", "mariel", "isabella"]
+
+print(family[3])
+print(family)
+family[0]= "karen"
+family[2] = "mariel"
+family[3] = "isabella"
+
+for family in family:
+    print(f"Hello {family}")
+
+ 
