c0a96e28a9bb0ce937dcf73186c1e59ee28042fb
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-09-23 20:55:35 +0000

    fuzz ball

--- PRACTICES/Fuzz_Ball.py
+++ PRACTICES/Fuzz_Ball.py
@@ -1,8 +1,11 @@
 #IG 6th Fuzz Ball 
 
-for x in range(1,50):
+for x in range(1,51):
     if x%3 == 0 and x%5 ==0:
-        print("fizzbuzz")
-    
-elif x%3 ==0
- 
+        print("FizzBuzz")
+    elif x%3 == 0:
+        print("Fizz")
+    elif x%5 ==0:
+        print("Buzz")
+    else:
+        print(x)
