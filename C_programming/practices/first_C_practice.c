bd37d7ed7e205373a221afd2fda1e950c1f4b17b
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-10-06 20:42:21 -0600

    Update and rename variables_practice.c to first_C_practice.c

---
+++ C_programming/practices/first_C_practice.c
@@ -0,0 +1,14 @@
+// IG 6th variables practice
+
+#include <stdio.h>
+
+int main() {
+    char name[30];
+
+    printf("Please enter your name: ");
+    scanf("%s", &name);  
+
+    printf("Hello, %s! Welcome to your first C program.\n", namee);
+
+    return 0;
+}
