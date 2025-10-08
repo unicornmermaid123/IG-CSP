b0299e5093b32a284b77063cf65e7e4d9fb3b24c
unicornmermaid123 <isabella.german@ucas-edu.net>
2025-10-08 13:44:06 -0600

    expressions practice

---
+++ C_programming/practices/Expressions_Practice.c
@@ -0,0 +1,19 @@
+//IG 6th Expressions Practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    printf(" 7 - 24/8 * 4 + 6 %d\n", 7 - 24/8 * 4 + 6);
+    printf(" 18 / 3 - 7 + 2 * 5 %d\n", 18 / 3 - 7 + 2 * 5);
+    printf("6 * 4 / 12 + 72  / 8 - 9 %d\n", 6 * 4 / 12 + 72  / 8 - 9 );
+    printf("(17 - 6 / 2) + 4 *3%d\n", (17 - 6 / 2) + 4 *3);
+    printf("-2(1 * 4 - 2 / 2) + (6 + 2 - 3)%d/n",-2(1 * 4 - 2 / 2) + (6 + 2 - 3) );
+    primtf("-1 * [(3 - 4 * 7) / 5] - 2 * ");
+
+
+
+   
+    return 0;
+}
